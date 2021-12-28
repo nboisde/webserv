cbd63dffdcf68db143e86a83572d950400b65c68
Nastasia Demont <nastasia@demont.me>
2021-12-28 16:03:35 +0100

    Test creating & binding sockets

---
+++ ndemont/test.hpp
@@ -0,0 +1,11 @@
+#ifndef TEST_HPP
+# define TEST_HPP
+
+# include <cstdio>
+# include <iostream>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <functional>
+
+#endif
