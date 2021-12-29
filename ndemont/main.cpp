518a52452b5d541cc52f27e560e306952f2aa6d5
Nastasia Demont <nastasia@demont.me>
2021-12-29 10:36:07 +0100

    Read and write from/on the socket

--- ndemont/main.cpp
+++ ndemont/main.cpp
@@ -1,5 +1,9 @@
 #include "Socket.hpp"
 #include "listenSocket.hpp"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
 
 int	main()
 {
@@ -8,5 +12,16 @@ int	main()
 
 	L_socket.listenning();
 	C_socket = L_socket.accepting();
+
+
+	char buffer[1024] = {0};
+	int valread = read(C_socket, buffer, 1024); 
+	printf("%s\n",buffer );
+	if(valread < 0)
+	{ 
+		printf("No bytes are there to read");
+	}
+	char *hello = (char *)("Hello from the server\n");//IMPORTANT! WE WILL GET TO IT
+	write(C_socket, hello , strlen(hello));
 	return (0);
 }
