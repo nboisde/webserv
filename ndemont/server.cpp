116ca9c1e9063e4dd659bec2cd1a49c140be3fc5
Nastasia Demont <nastasia@demont.me>
2022-01-03 10:24:42 +0100

    Clean mess

---
+++ ndemont/server.cpp
@@ -0,0 +1,136 @@
+#include "socket.hpp"
+#include "listenSocket.hpp"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+# define BUFFER 4096
+
+struct pollfd &new_pollfd(int socketfd) //PROTEGER MEMOIRE
+{
+	struct pollfd *ptr = new struct pollfd;
+	ptr->fd = socketfd;
+	ptr->events = POLLIN;
+	return (*ptr);
+}
+
+int	launch_server()
+{
+	ws::listenSocket			listenSocket;
+	//char						*hello = (char *)("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8;\r\nContent-Length: 21\r\n\r\n<h1>Hello world!</h1>\n");
+	int							delai = -1;
+	std::vector<struct pollfd>	pollfd;
+	int							ret;
+	int							end_server = 0;
+	int							new_sd = -1;
+	int							close_conn, compress_array = 0;
+	char						buffer[80];
+
+	int status = fcntl(listenSocket.server_fd, F_SETFL, fcntl(listenSocket.server_fd, F_GETFL, 0) | O_NONBLOCK);
+	if (status < 0)
+	{
+		perror("In fcntl: ");
+		exit(EXIT_FAILURE);
+	}
+	listenSocket.listenning();
+	pollfd.push_back(new_pollfd(listenSocket.server_fd));
+	do
+	{
+		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+		ret = poll(&(*pollfd.begin()), pollfd.size(), delai);
+		if (ret < 0)
+		{
+			perror("In poll: failes");
+			break;
+		}
+		if (ret == 0)
+		{
+			perror("In poll: timed out");
+			break;
+		}
+		for (int i = 0; i < (int)pollfd.size(); i++)
+		{
+			if (pollfd[i].revents == 0)
+				continue;
+			if (pollfd[i].revents != POLLIN)
+			{
+				printf("  Error! revents = %d\n", pollfd[i].revents);
+				end_server = 1;
+				break;
+			}
+			if (pollfd[i].fd == listenSocket.server_fd)
+			{
+				printf("Listening socket is readable\n");
+				do 
+				{
+					struct sockaddr_in cli_addr;
+					socklen_t	addrlen = sizeof(cli_addr);
+
+					std::cout << "Before Accept\n";
+					new_sd = accept(listenSocket.server_fd, (struct sockaddr *)&cli_addr, (socklen_t*)&addrlen);
+					std::cout << "new fd = " << new_sd << std::endl;
+					if (new_sd < 0)
+					{
+						if (errno != EWOULDBLOCK)
+						{
+							perror("  accept() failed");
+							end_server = 1;
+						}
+						break;
+					}
+					printf("  New incoming connection - %d\n", new_sd);
+					pollfd.push_back(new_pollfd(new_sd));
+				} while (new_sd != -1);
+			}
+			else
+			{
+				printf("  Descriptor %d is readable\n", pollfd[i].fd);
+				close_conn = 0;
+				do
+				{
+					ret = recv(pollfd[i].fd, buffer, sizeof(buffer), 0);
+					if (ret < 0)
+					{
+						perror("In recv: fail");
+						if (errno != EWOULDBLOCK)
+						{
+						close_conn = 1;
+						}
+						break;
+					}
+					if (ret == 0)
+					{
+						std::cout << "Connection closed\n";
+						close_conn = 1;
+						break;
+					}
+					int len = ret;
+					printf("  %d bytes received\n", len);
+					ret = send(pollfd[i].fd, buffer, len, 0);
+					if (ret < 0)
+					{
+						perror("  send() failed");
+						close_conn = 1;
+						break;
+					}
+
+				} while (1);
+				if (close_conn)
+				{
+					close(pollfd[i].fd);
+					pollfd[i].fd = -1;
+					compress_array = 1;
+				}
+			}
+		}
+		if (compress_array)
+		{
+			for (int i = 0; i < (int)pollfd.size(); i++)
+			{
+				if (pollfd[i].fd == -1)
+					pollfd.erase(pollfd.begin() + i);
+			}
+		}
+	} while (end_server == 0);
+	return (0);
+}
---
+++ sockets/basicSocket 2.cpp
@@ -0,0 +1,31 @@
+#include "basicSocket.hpp"
+
+WS::basicSocket::basicSocket(){}
+
+WS::basicSocket::basicSocket(int domain, int service, int protocol, int port, unsigned long interface): domain(domain), service(service), protocol(protocol){
+	// Defining address structure.
+	this->address.sin_family = this->domain;
+	this->address.sin_port = htons(port);
+	this->address.sin_addr.s_addr = htonl(interface);
+
+	// Establish connection via socket.
+	this->sock = socket(this->domain, this->service, this->protocol);
+	//this->connection = networkConnection(this->sock, this->address);
+	//testConnection(this->connection);
+}
+
+WS::basicSocket::~basicSocket(){}
+
+// Confirm that connection is well established.
+void	WS::basicSocket::testConnection(int item_to_test){
+	if (item_to_test < 0)
+	{
+		perror("Failed to connect");
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Getters
+struct sockaddr_in WS::basicSocket::getAddress() const{ return this->address; }
+int WS::basicSocket::getSock() const{ return this->sock; }
+int WS::basicSocket::getConnection() const{ return this->connection; }
