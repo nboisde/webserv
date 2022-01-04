e578969e87254ace0fe0059df893404652d0ed09
Nastasia Demont <ndemont@student.42.fr>
2022-01-04 15:33:05 +0100

    Conflict

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -1,8 +1,21 @@
 #ifndef WEBSERV_HPP
 # define WEBSERV_HPP
 
+// General MACRORS
 # define DELAI -1
 # define BUFFER_SIZE 4096
+
+// Requests State MACROS
+# define REQUEST_FORMAT_ERROR -1
+# define RECIEVING_HEADER 0
+# define HEADER_RECIEVED 1
+# define BODY_RECIEVED 2
+
+// Methods MACROS
+# define UNKNOWN 0
+# define GET 1
+# define POST 2
+# define DELETE 3
 
 //C_LIBRARIES//
 # include <stdlib.h>
@@ -23,7 +36,7 @@
 # include <vector>
 
 //CLASSES//
-# include "Server.hpp"
+# include "server.hpp"
 # include "requests.hpp"
 # include "socket.hpp"
 # include "listenSocket.hpp"
