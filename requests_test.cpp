f3efbc11241773e7d79ab4a5e67b827dc7c7dd20
Nicolas BOISDE <nboisde@e1r10p5.42.fr>
2022-01-04 13:37:47 +0100

    tested with with empty body : TODO-> case sensitive on content-length variable.

--- requests_test.cpp
+++ requests_test.cpp
@@ -3,22 +3,25 @@
 #include "requests.hpp"
 #include <vector>
 
-#define BUFFER_SIZE 1
+#define BUFFER_SIZE 100000000
+#define RC goo
 
 int main(void)
 {
-	std::string buf1 = "POST / HTTP/1.1\r\nContent-Type: application/json\r\ncache-control: no-cache\r\nPostman-Token: 47124fa0-d97e-4b6f-bfbe-a45c89fb6298\r\nUser-Agent: PostmanRuntime/7.1.1\r\nAccept: */*\r\nHost: localhost:8080\r\naccept-encoding: gzip, deflate\r\ncontent-length: 56\r\nConnection: keep-alive\r\n\r\n{\r\n\t\"email\": \"test@example.com\",\r\n\t\"password\":\"azery\"\r\n}";
-	std::cout << buf1 << std::endl;
+	//std::string buf1 = "POST / HTTP/1.1\r\nContent-Type: application/json\r\ncache-control: no-cache\r\nPostman-Token: 47124fa0-d97e-4b6f-bfbe-a45c89fb6298\r\nUser-Agent: PostmanRuntime/7.1.1\r\nAccept: */*\r\nHost: localhost:8080\r\naccept-encoding: gzip, deflate\r\ncontent-length: 56\r\nConnection: keep-alive\r\n\r\n{\r\n\t\"email\": \"test@example.com\",\r\n\t\"password\":\"azery\"\r\n}";
+	std::string goo = "GET /favicon.ico HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nConnection: keep-alive\r\nsec-ch-ua: \" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\", \"Google Chrome\";v=\"96\"\r\nDNT: 1\r\nsec-ch-ua-mobile: ?0\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36\r\nsec-ch-ua-platform: \"macOS\"\r\nAccept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8\r\nSec-Fetch-Site: same-origin\r\nSec-Fetch-Mode: no-cors\r\nSec-Fetch-Dest: image\r\nReferer: http://127.0.0.1:8080/\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7\r\n\r\n";
+	
+	std::cout << RC << std::endl;
 
 	std::vector<std::string> v;
 	size_t i = 0;
-	while (i < buf1.length())
+	while (i < RC.length())
 	{
 		int j = 0;
 		std::string s;
-		while (j < BUFFER_SIZE && buf1[i + j])
+		while (j < BUFFER_SIZE && RC[i + j])
 		{
-			s += buf1[i + j];
+			s += RC[i + j];
 			j++;
 		}
 		v.push_back(s);
@@ -38,13 +41,16 @@ int main(void)
 		//	break;
 		it++;
 	}
-	std::cout << "state: " << r.requestReceptionState() << std::endl;
-	std::cout << "raw: " << std::endl << r.getRawContent() << std::endl;
-	std::cout << "Method (1-get, 2-post, 3-del)"<< r.getMethodType() << std::endl;
-	std::cout << "content length: " << r.getContentLength() << std::endl;
+	std::cout << "RAW_INFORMATIONS: " << std::endl << "----------------------------------------" << std::endl;
+	std::cout << " - state: " << r.requestReceptionState() << std::endl;
+	std::cout << " - raw content: " << std::endl << r.getRawContent() << std::endl;
+	std::cout << " - Method (1-get, 2-post, 3-del)"<< r.getMethodType() << std::endl;
+	std::cout << " - content length: " << r.getContentLength() << std::endl;
+	std::cout << "////////////////////////////////////////////////////////" << std::endl;
 
-	//r.fillHeaderAndBody();
-	std::cout << r.getHeader() << std::endl;
+	std::cout << "HEADER AND BODY: " << std::endl << "----------------------------------------" << std::endl;
+	r.fillHeaderAndBody();
+	std::cout << " - header : " << std::endl << r.getHeader() << std::endl;
 	std::cout << "--------------------" << std::endl;
-	std::cout << r.getBody() << std::endl;
+	std::cout << " - body : " << std::endl << r.getBody() << std::endl;
 }
