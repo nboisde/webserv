95fc1a2e06be0dba96a0e8dd487475c4e0895fb5
Guillaume PETIT <gpetit@e1r8p6.42.fr>
2022-01-10 18:23:27 +0100

    Response, debut de classe

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -7,16 +7,31 @@ namespace ws
 {
 	class Response {
 		public:
+			static std::map<int, std::string>	_status_code;
+
 			Response( void );
 			Response( Response const & src );
 			virtual ~Response();
 
 			Response &		operator=( Response const & rhs );
 
+			std::string 	genStatusLine( void );
+
+
+			char*			response( void ); //CREER LA REPONSE A PARTIR DE ENTETE ET DONNES DE RETOUR
+			size_t			response_size( void ); //RENVOIE LA TAILLE DE LA REP
+
 			std::string		getResponse( void ) const;
-			
+			std::string		getDate( void ) const;
+			std::string		getStatusLine(void) const;
+			void			setResponse( void );
+			void			setDate( void );
+			void			setStatusLine(void);
+	
 		private:
-			std::string		_response;
+			std::string 	_status_line;
+			std::string		_date;
+            std::string		_response;
 	};
 }
 #endif
--- sources/Response.cpp
+++ sources/Response.cpp
@@ -3,27 +3,78 @@
 namespace ws
 {
 
-Response::Response( void ) : _response("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: 76\r\n\r\n<!DOCTYPE html><html><body><h1>First display on localhost</h1></body></html>\r\n\r\n")
-{
+std::map<int, std::string>  Response::_status_code = {
+    (100, "Continue"), 
+    (200, "OK"),
+    (300, "Multiple Choice"),
+    (301, "Moved Permanently"),
+    (400 "Bad Request"),
+    (401 "Unauthorized"),
+    (403 "Forbidden"),
+    (404 "Not Found"),
+    (500, "Internal Server Error")
 }
+
+Response::Response( void ) : _response("Hello from Server\n")
+{
+
+};
 
 Response::Response( Response const & src)
 {
     *this = src;
 }
 
-Response::~Response() {}
+Response::~Response( void )
+{
+
+};
 
 Response &  Response::operator=( Response const & rhs)
 {
     if ( this != &rhs )
         _response = rhs.getResponse();
+        _status_line = rhs.getStatusLine();
+        _date = rhs.getDate();
     return *this;
 }
 
-std::string Response::getResponse() const
-{
+//METHODS - //
+
+char*      Response::response( void ){
+    //CONCATENER LES STRINGS POUR CREER LA REPONSE
+    return response.c_str();
+}
+
+size_t      Response::response_size( void ){
+    return  response.size();
+}
+
+//ACCESSORS - GETTERS//
+
+std::string Response::getResponse( void ) const{
     return _response;
+}
+
+std::string		Response::getDate( void ) const{
+    return _date;
+}
+
+std::string		Response::getStatusLine(void) const{
+    return _status_line;
+}
+
+//ACCESSORS - SETTERS//
+void			Response::setResponse( std::string resp ){
+    _response = resp;
+}
+
+void			Response::setDate( std::string date ){
+    _date = date;
+}
+
+void			Response::setStatusLine( std::string status_l ){
+    _status_line = status_l;
 }
 
 }
