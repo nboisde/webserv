8121aa5792092b870a127fa8383c7e1f813bf6d8
Guillaume PETIT <gpetit@e1r8p6.42.fr>
2022-01-10 18:23:27 +0100

    Response, debut de classe

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -9,15 +9,30 @@ namespace ws
 
 	class Response {
 		public:
+			static std::map<int, std::string>	_status_code;
+
 			Response( void );
 			Response( Response const & src );
 			virtual ~Response();
 
 			Response &		operator=( Response const & rhs );
 
+			std::string 	genStatusLine( void );
+
+
+			char*			response( void ); //CREER LA REPONSE A PARTIR DE ENTETE ET DONNES DE RETOUR
+			size_t			response_size( void ); //RENVOIE LA TAILLE DE LA REP
+
 			std::string		getResponse( void ) const;
-			
+			std::string		getDate( void ) const;
+			std::string		getStatusLine(void) const;
+			void			setResponse( void );
+			void			setDate( void );
+			void			setStatusLine(void);
+	
 		private:
+			std::string 	_status_line;
+			std::string		_date;
             std::string		_response;
 	};
 }
