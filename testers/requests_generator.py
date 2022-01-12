0c128af0eef49afb73e865a96a7ac35260c40648
Nicolas BOISDE <nboisde@e3r12p15.42.fr>
2022-01-12 20:29:00 +0100

    python script for generating headers, bizarre, fildes dosen't change

--- testers/requests_generator.py
+++ testers/requests_generator.py
@@ -6,7 +6,7 @@ import json
 
 
 HTTPConnection._http_vsn_str = "HTTP/1.1"
-url = "http://127.0.0.1:8080"
+url = "http://127.0.0.1:8080/chunked"
 files = {
 	'data' : 'HelloWorld',
 	'document' : open(os.path.join('../tmp/lorem.txt'), "rb")
@@ -19,14 +19,24 @@ headers = {
 'Cache-Control': "no-cache",
 }
 
+def gen():
+	yield '6\r\ncoucou\r\n'
+	yield '5\r\nmonde\r\n0\r\n\r\n'
+
 i = 0
 while i < 100:
+	headers = {
+'Transfer-Encoding': "chunked",
+'Accept': "multipart/form-data",
+'Content-Type': "application/pdf",
+'Cache-Control': "no-cache",
+}
 	files = {
 	'data' : 'HelloWorld',
 	'document' : open(os.path.join('../tmp/lorem.txt'), "rb")
 	}
-	r = rq.post(url, files=files)#, headers=headers)
-	time.sleep(0.01)
+	r = rq.post('http://127.0.0.1:8080/chunked', files=files)#, headers=headers)
+	#time.sleep(0.01)
 	i+=1
 
 #r = rq.post(url, files=files)
