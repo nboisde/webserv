3ad1137cedf5db08fc24ad25ae80ea49233fbf0a
Nastasia Demont <nastasia@demont.me>
2022-01-12 22:51:44 +0100

    Develop parsing - begin for each section

--- sources/Parsing.cpp
+++ sources/Parsing.cpp
@@ -22,15 +22,83 @@ int		readFile(char *file, std::string *content)
 	std::string		buffer;
 
 	while (getline(ifs, buffer))
+	{
 		*content += buffer;
+		*content += "\n";
+	}
 	ifs.close();
 	return (1);
 }
 
+int		checkHttpBegin(std::string content, int i, int size)
+{
+	while (i < size && isspace(content[i]))
+		i++;
+	i = (&(content[i])).find("http");
+	if (i != 0)
+		return (0);
+	i = 4;
+	while (i < size && isspace(content[i]))
+		i++;
+	if (content[i] != '{')
+		return (0);
+	return (i);
+}
+
+int		checkHttpEnd(std::string content, int pos, int size)
+{
+	while (pos < size && isspace(content[pos]))
+		pos++;
+	if (content[pos] != '}')
+		return (0);
+	while (pos < size && isspace(content[pos]))
+		pos++;
+	if (pos != size)
+		return (0);
+	return (pos);
+}
+
+int		checkServer(std::string content, int i, int size)
+{
+	int	pos;
+
+	while (i < size && isspace(content[i]))
+		i++;
+	pos = (&(content[i])).find("server");
+	if (pos != 0)
+		return (0);
+	i += 6;
+	while (i < size)
+	{
+		i = checkInfo(content, i, size);
+		if (!i)
+			return (0);
+		while (i < size && isspace(content[i]))
+			i++;
+		if (content[i] == '}')
+			break;
+	}
+	if (i == size && content[i] != '}')
+		return (0);
+	return (i);
+}
+
 int		initServer(std::string content, ws::Server *server)
 {
-	(void)content;
+	int	size = content.size();
+	int	pos = 0;
+
 	(void)server;
+	if (!(pos = checkHttpBegin(content, pos, size)))
+		return (0);
+	while (pos < size)
+	{
+		if (!(pos = checkServer(content, pos, size)))
+			return (0);
+		pos++;
+	}
+	//if ((!checkHttpEnd(content, pos, size)))
+	//	return (0);
 	return (1);
 }
 
