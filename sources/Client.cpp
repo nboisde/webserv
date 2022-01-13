25ac3144b5d13c5adbfc96d0aa827cb24d310522
nboisde <nboisde.d3v@gmail.com>
2022-01-13 11:02:57 +0100

    cleaning Request file.

--- sources/Client.cpp
+++ sources/Client.cpp
@@ -65,12 +65,12 @@ int Client::receive(void)
 		//std::cout << "ret :" << ret << std::endl;
 		_req.fillHeaderAndBody();
  		//std::cout << _req.getRawContent() << std::endl;
-		std::cout << "--------------------------------" << std::endl << "Header:" << std::endl;
-		std::cout << _req.getHeader() << std::endl;
+		//std::cout << "--------------------------------" << std::endl << "Header:" << std::endl;
+		//std::cout << _req.getHeader() << std::endl;
 		//std::cout << "Body : " << std::endl;
 		//std::cout << _req.getBody();// << std::endl;
 		//std::cout << _req.getBody().length() << std::endl;
-		//write(1, _req.getBody().c_str(), _req.getBody().length());
+		write(1, _req.getBody().c_str(), _req.getBody().length());
 		//std::cout << "The connection id is: " << _fd << std::endl;
 		return WRITING;
 	}
