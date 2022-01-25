f69a9dc5cf5206c598a438c492649d989930a37b
nboisde <nboisde.d3v@gmail.com>
2022-01-25 12:48:58 +0100

    Signal handler, must exit clean (check leaks after.)

---
+++ sources/Signals.cpp
@@ -0,0 +1,10 @@
+#include "webserv.hpp"
+
+void	signalHandler(int signum)
+{
+	std::cout << std::endl << GREEN << "Closing the server..." << RESET << std::endl;
+	// Clean exit..
+	sleep(1);	
+	std::cout << GREEN << "Bye bye" << RESET << std::endl;
+	exit(signum);
+}
--- sources/main.cpp
+++ sources/main.cpp
@@ -5,7 +5,8 @@
 int	main(int ac, char **av)
 {
 	ws::Parser	parser;
-	
+
+	std::signal(SIGINT, signalHandler);
 	if (ac != 2)
 	{
 		std::cout << "Missing valid config file\n";
