4259e8bc7a9a3049c4333c9b52497cc9f1fc15ca
Nastasia Demont <ndemont@student.42.fr>
2022-01-27 14:18:29 +0100

    Include favicon for all pages

--- www/php/var.php
+++ www/php/var.php
@@ -1,3 +1,7 @@
+<head>
+	<link rel="shortcut icon" type="image/x-icon" href="https://favicon-generator.org/favicon-generator/htdocs/favicons/2014-12-18/32e6e83989d480f4bed129837dec3f6c.ico" />
+</head>
+
 <?php
     while (list($var,$value) = each ($_SERVER)) {
         echo "$var => $value <br />";
