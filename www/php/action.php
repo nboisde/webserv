3d37c0235c68ff35aa173f6e2d04d20942a6d120
Nastasia Demont <nastasia@demont.me>
2022-01-28 19:32:48 +0100

    develop website

--- www/php/action.php
+++ www/php/action.php
@@ -1,8 +1,3 @@
-<head>
-        <link rel="shortcut icon" type="image/x-icon" href="https://favicon-generator.org/favicon-generator/htdocs/favicons/2014-12-18/32e6e83989d480f4bed129837dec3f6c.ico" />
-        <title>Welcome</title>
-	    <link rel="stylesheet" href="style.css">
-</head>
 
 Bonjour et bienvenue, <?php echo htmlspecialchars($_POST['nom']); ?>.
 Tu as <?php echo (int)$_POST['age']; ?> ans.
