97736e105919817fdabd8b199c1d9ddfd081cb33
Nastasia Demont <ndemont@student.42.fr>
2022-01-28 16:15:29 +0100

    GLamourous server:

--- www/php/action.php
+++ www/php/action.php
@@ -1,2 +1,8 @@
-Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.
+<head>
+        <link rel="shortcut icon" type="image/x-icon" href="https://favicon-generator.org/favicon-generator/htdocs/favicons/2014-12-18/32e6e83989d480f4bed129837dec3f6c.ico" />
+        <title>Welcome</title>
+	    <link rel="stylesheet" href="style.css">
+</head>
+
+Bonjour et bienvenue, <?php echo htmlspecialchars($_POST['nom']); ?>.
 Tu as <?php echo (int)$_POST['age']; ?> ans.
