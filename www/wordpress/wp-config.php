46cb6fb4177243cd73d52f677112e85f039a16a1
Guillaume Petit <gpetit@student.42.fr>
2022-02-02 14:26:41 +0100

    no progress, waiting for nas to patch

--- www/wordpress/wp-config.php
+++ www/wordpress/wp-config.php
@@ -31,7 +31,7 @@ define( 'DB_USER', 'user' );
 define( 'DB_PASSWORD', 'password' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost:3306' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8' );
