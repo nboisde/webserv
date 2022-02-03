fd20b6e1744004611d6cc9cc4a4c495d544daa68
Nastasia Demont <ndemont@student.42.fr>
2022-02-03 15:02:04 +0100

    Correct allowed default conf for no method

--- www/php/agent.php
+++ www/php/agent.php
@@ -1,7 +1,12 @@
 <head>
         <link rel="shortcut icon" type="image/x-icon" href="/imgs/fire.ico" />
         <title>404 Error</title>
-	    <link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/style.css">
+        <ul>
+		<li><a href="/html/contact.html">Contact</a></li>
+		<li><a href="/html/about.html">About</a></li>
+		<li><a href="/html/index.html">Home</a></li>
+	</ul>
 </head>
 <?php
 echo $_SERVER['HTTP_USER_AGENT'];
