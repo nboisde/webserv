29b911e172d9e0862323d5421fabc4cc628d0469
Nastasia Demont <ndemont@student.42.fr>
2022-02-04 13:55:11 +0100

    Edit Architecture for futur correction

--- www/php/profile.php
+++ www/php/profile.php
@@ -1,11 +1,11 @@
 <head>
-        <link rel="shortcut icon" type="image/x-icon" href="/imgs/fire.ico" />
-        <title>Action</title>
+	<link rel="shortcut icon" type="image/x-icon" href="/imgs/fire.ico"/>
+	<title>Contact</title>
 	<link rel="stylesheet" href="/css/style.css">
 	<ul>
 		<li><a href="/html/contact.html">Contact</a></li>
 		<li><a href="/html/about.html">About</a></li>
-		<li><a href="/php/profile.php">Profile</a></li>
+		<li><a style="text-decoration: underline;" href="/php/profile.php">Profile</a></li>
 		<li><a href="/html/index.html">Home</a></li>
 	</ul>
 </head>
