a67570018c6600cdfd40717192e40e12eda70fdd
Nastasia Demont <ndemont@student.42.fr>
2022-02-08 16:51:02 +0100

    Change error pages

--- www/php/405.php
+++ www/php/405.php
@@ -1,10 +1,14 @@
-<!DOCTYPE html>
-<html>
-<header>
+
+	<head>
+<link rel="shortcut icon" type="image/x-icon" href="/imgs/fire.ico"/>
+	<title>404</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+	<body>
+	<header>
 		<?php require('header.php'); ?>
 	</header>
-	<body>
-		<h1>405</h1>
+	<h1>405</h1>
 		<p>Not Allowed</p>
+	<img src="/imgs/you-shall-not-pass.gif" alt="Gandalf">
 	</body>
-</html>
--- www/php/index.php
+++ www/php/index.php
@@ -4,11 +4,11 @@
 	<title>Homepage</title>
 	<link rel="stylesheet" href="/css/style.css">
 </head>
-<header>
-	<?php include('header.php');
-	?>
-</header>
+
 <body>
+	<header>
+		<?php include('header.php'); ?>
+	</header>
 	<h1>Welcome to our<br> Webserver</h1>
 	<div class="content">
 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
