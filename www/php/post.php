b9aa01cc52eb891567b46682d36016512cd65ead
Nastasia Demont <ndemont@student.42.fr>
2022-02-09 14:16:29 +0100

    Change post page

--- www/php/post.php
+++ www/php/post.php
@@ -8,19 +8,22 @@
 	<header>
 		<?php require('header.php'); ?>
 	</header>
-	<img src="/imgs/cute.png">
-	<h2>Fill in your profile</h2>
-	<form action="/php/profile.php" method="post">	
+	<div class="avatar">
+		<img src="/imgs/default.png">
+	</div>
+	<h2>Change your profile picture</h2>
+	<form action="http://localhost:8080" method="post" enctype="multipart/form-data">
+		<p><input type="file" id="myFile" name="filename" hidden>
+		<label for="myFile">Upload</label>
+		<span id="file-chosen">No file chosen</span>
+		<p><button class="button" type="submit">Submit</button>
+		<h2>Fill in your profile</h2>
 		<p>First name <input type="text" name="firstname" /></p>
 		<p>Last name <input type="text" name="lastname" /></p>
 		<p>Login <input type="text" name="login" /></p>
 		<p><input type="submit" value="OK"></p>
 	</form>
-	<h2>Upload your picture</h2>
-	<form action="http://localhost:8080" method="post" enctype="multipart/form-data">
-		<p><input type="file" name="file1">
-		<p><button class="styled" type="submit">Submit</button>
-	</form>
+
 </body>
 <footer>
 	<?php include('footer.php');
--- www/php/profile.php
+++ www/php/profile.php
@@ -1,16 +1,24 @@
 <head>
+<link rel="shortcut icon" type="image/x-icon" href="/imgs/favicon.ico"/>
+	<title>Profile</title>
 	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
 <header>
 	<?php require('header.php'); ?>
 </header>
-<img src="/imgs/cute.png">
+	<div class="avatar">
+		<img src="/imgs/default.png">
+	</div>
 	<form action="action.php" method="post">
-		<p>Hello and welcome <?php echo htmlspecialchars($_POST['firstname']); ?> <?php echo htmlspecialchars($_POST['lastname']); ?></p>
-		<p>(aka <?php echo htmlspecialchars($_POST['login']); ?>)</p>
-		<p><input type="submit" value="OK"></p>
+		<h1>Welcome to your profile</h1>
+		<p>FIRST NAME <?php echo htmlspecialchars($_POST['firstname']);?></p>
+		<p>LAST NAME <?php echo htmlspecialchars($_POST['lastname']); ?></p>
+		<p>LOGIN <?php echo htmlspecialchars($_POST['login']);?></p>
 	</form>
+	<div>
+		<a href="/php/post.php" class="button">EDIT</a>
+	</div>
 </body>
 <footer>
 	<?php include('footer.php');
